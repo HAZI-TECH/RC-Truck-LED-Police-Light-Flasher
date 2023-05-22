c91083992958a283a94733f6a97f3388d4feffce
Hasitha Jayasundara <134243522+HAZI-TECH@users.noreply.github.com>
2023-05-22 20:30:23 +0530

    Add files via upload

---
+++ RC_Truck_VIP_Light.ino
@@ -0,0 +1,389 @@
+// HAZI TECH
+// Programmed by Hasitha Jayasundara
+// Visit my YouTube Channel for more info - http://www.youtube.com/c/HAZITECH?sub_confirmation=1
+
+#define LED_PIN1 1 //LED CH1
+#define LED_PIN2 2 //LED CH2
+#define LED_PIN3 3 //LED CH3
+#define LED_PIN4 4 //LED CH4
+
+int SPEED = 60;
+
+void setup() 
+{
+pinMode(LED_PIN1, OUTPUT);
+pinMode(LED_PIN2, OUTPUT);
+pinMode(LED_PIN3, OUTPUT);
+pinMode(LED_PIN4, OUTPUT);
+}
+
+void loop() 
+{
+HAZI_TECH_VIP_1();
+HAZI_TECH_VIP_1();
+HAZI_TECH_VIP_1();
+
+HAZI_TECH_VIP_2();
+HAZI_TECH_VIP_2();
+HAZI_TECH_VIP_2();
+
+HAZI_TECH_VIP_3();
+HAZI_TECH_VIP_3();
+HAZI_TECH_VIP_3();
+
+HAZI_TECH_VIP_4();
+HAZI_TECH_VIP_4();
+
+HAZI_TECH_VIP_5();
+HAZI_TECH_VIP_5();
+HAZI_TECH_VIP_5();
+HAZI_TECH_VIP_5();
+HAZI_TECH_VIP_5();
+
+HAZI_TECH_VIP_6();
+HAZI_TECH_VIP_6();
+HAZI_TECH_VIP_6();
+HAZI_TECH_VIP_6();
+HAZI_TECH_VIP_6();
+
+HAZI_TECH_VIP_7();
+HAZI_TECH_VIP_7();
+HAZI_TECH_VIP_7();
+HAZI_TECH_VIP_7();
+HAZI_TECH_VIP_7();
+}
+
+// HAZI TECH
+// Programmed by Hasitha Jayasundara
+// Visit my YouTube Channel for more info - http://www.youtube.com/c/HAZITECH?sub_confirmation=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void HAZI_TECH_VIP_1()
+{
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+}
+
+void HAZI_TECH_VIP_2()
+{
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+}
+
+void HAZI_TECH_VIP_3()
+{
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+}
+
+void HAZI_TECH_VIP_4()
+{
+digitalWrite(LED_PIN1, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN1, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+}
+
+void HAZI_TECH_VIP_5()
+{
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN3, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN3, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN2, LOW);
+digitalWrite(LED_PIN3, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+delay (SPEED);
+delay (SPEED);
+}
+
+void HAZI_TECH_VIP_6()
+{
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN1, LOW);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN4, HIGH);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN2, HIGH);
+digitalWrite(LED_PIN3, LOW);
+delay (SPEED);
+delay (SPEED);
+}
+
+void HAZI_TECH_VIP_7()
+{
+digitalWrite(LED_PIN1, HIGH);
+digitalWrite(LED_PIN2, HIGH);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN1, LOW);
+digitalWrite(LED_PIN2, LOW);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN3, HIGH);
+digitalWrite(LED_PIN4, HIGH);
+delay (SPEED);
+delay (SPEED);
+digitalWrite(LED_PIN3, LOW);
+digitalWrite(LED_PIN4, LOW);
+delay (SPEED);
+delay (SPEED);
+}
